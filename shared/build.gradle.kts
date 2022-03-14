764ae8773ccffc7010757274eb4ca8c27c64e3ee
Amirhussein soori <amirhusseinsppro75@gmail.com>
2022-03-14 23:41:05 +0330

    add ktor and sqldelight to repository :)

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -23,18 +23,60 @@ kotlin {
     }
     
     sourceSets {
-        val commonMain by getting
+        val commonMain by getting{
+            dependencies {
+                implementation("io.ktor:ktor-client-core:1.6.7")
+                implementation("io.ktor:ktor-client-serialization:1.6.7")
+                implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.3.2")
+                implementation("org.jetbrains.kotlin:kotlin-serialization:1.6.10")
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.0")
+                implementation("co.touchlab:stately-common:1.2.1")
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
+                implementation("com.squareup.sqldelight:runtime:1.5.3")
+            }
+        }
         val commonTest by getting {
             dependencies {
                 implementation(kotlin("test"))
             }
         }
-        val androidMain by getting
-        val androidTest by getting
-        val iosX64Main by getting
-        val iosArm64Main by getting
-        val iosSimulatorArm64Main by getting
+        val androidMain by getting{
+            dependencies {
+                implementation("io.ktor:ktor-client-android:1.6.7")
+                implementation("com.squareup.sqldelight:android-driver:1.5.3")
+            }
+        }
+        val androidTest by getting{
+            dependencies {
+                implementation("androidx.test:runner:1.4.0")
+                implementation("junit:junit:4.13.2")
+            }
+        }
+        val iosX64Main by getting{
+            dependencies {
+                implementation("io.ktor:ktor-client-ios:1.6.7")
+                implementation("com.squareup.sqldelight:native-driver:1.5.3")
+            }
+        }
+        val iosArm64Main by getting{
+            dependencies {
+                implementation("io.ktor:ktor-client-ios:1.6.7")
+                implementation("com.squareup.sqldelight:native-driver:1.5.3")
+
+            }
+        }
+        val iosSimulatorArm64Main by getting{
+            dependencies {
+                implementation("io.ktor:ktor-client-ios:1.6.7")
+                implementation("com.squareup.sqldelight:native-driver:1.5.3")
+
+            }
+        }
+
+
+
         val iosMain by creating {
+
             dependsOn(commonMain)
             iosX64Main.dependsOn(this)
             iosArm64Main.dependsOn(this)
